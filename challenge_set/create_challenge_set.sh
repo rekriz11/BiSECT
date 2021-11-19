ca211506ad437f4ad253c218ab14946d20ac82d9
rekriz11 <rekriz@seas.upenn.edu>
2021-11-19 15:03:04 -0500

    t

---
+++ challenge_set/create_challenge_set.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+source "/exp/rkriz/venv/bin/activate"
+
+DATA_FOLDER = $1
+OUTPUT_FOLDER = $2
+
+mkdir -p $OUTPUT_FOLDER
+
+python challenge_set/find_original_test.py \
+-data_folder $DATA_FOLDER \
+-output_folder $OUTPUT_FOLDER
+
+
+# ./challenge_set/create_challenge_set.sh /exp/rkriz/data/bisect/en-final/ /exp/rkriz/data/bisect/challenge_set/
---
+++ challenge_set/find_original_test.py
@@ -0,0 +1,44 @@
+import argparse
+
+def read_file(filepath):
+	data = []
+	with open(filepath) as f:
+		for line in f:
+			data.append(line.strip())
+	return data
+
+def write_output(data, output_path):
+	with open(output_path, 'w') as f:
+		for d in data:
+			f.write(d + "\n")
+
+def main(args):
+	train_complex = read_file(args.data_folder + "train.complex")
+	train_simple = read_file(args.data_folder + "train.complex")
+	train_dataset = read_file(args.data_folder + "train.dataset")
+
+	original_test = {"complex": [], "simple": []}
+	for i, tc in enumerate(train_complex):
+		ts, td = train_simple[i], train_dataset[i]
+		if td in ["jrc", "emea"]:
+			original_test["complex"].append(tc)
+			original_test["simple"].append(ts)
+
+	write_output(original_test['complex'], args.output_folder + "test_original.complex")
+	write_output(original_test['simple'], args.output_folder + "test_original.simple")
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Compute model score...')
+    parser.add_argument('-data_folder', type=str, required=True, help='Path to data')
+    parser.add_argument('-output_folder', type=str, required=True, help='Path to write output')
+    args = parser.parse_args()
+    main(args)
+
+
+'''
+python challenge_set/find_original_test.py \
+-data_folder /exp/rkriz/data/bisect/en-final/ \
+-output_folder /exp/rkriz/data/bisect/challenge_set/
+'''
